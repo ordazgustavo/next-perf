115c84cca779390ff5d3a40814c9ad16a9f0c507
Gustavo <ordazsgustavo@gmail.com>
2021-08-22 16:51:44 -0500

    Initial lighthouse ci setup

---
+++ lighthouserc.js
@@ -0,0 +1,14 @@
+const baseUrl = 'http://localhost:3000';
+const paths = ['/', '/isr', '/ssg', '/ssr', '/ssr-cache'];
+
+module.exports = {
+  ci: {
+    collect: {
+      url: paths.map(p => baseUrl + p),
+      startServerCommand: 'npm start',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
